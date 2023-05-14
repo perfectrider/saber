ee162bc2bcf59f4b4145d4d8b468fa19cfd7a2e6
perfectrider <konsgrebenev@gmail.com>
2023-05-14 20:35:09 +0300

    new(app): view refact

--- build_app/views.py
+++ build_app/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+
 from .utils import get_sorted_tasks
 
 from rest_framework.views import APIView
@@ -8,10 +10,14 @@ class BuildView(APIView):
     def post(self, request):
         build_name = request.data.get('build')
         if not build_name:
-            return Response({'error': 'No "build" parameter provided'},
-                            status=400)
+            return Response(
+                {'error': 'No "build" parameter provided'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         try:
             tasks = get_sorted_tasks(build_name)
             return Response({'tasks': tasks})
         except ValueError as e:
-            return Response({'error': str(e)}, status=400)
+            return Response(
+                {'error': str(e)}, status=status.HTTP_400_BAD_REQUEST
+            )
