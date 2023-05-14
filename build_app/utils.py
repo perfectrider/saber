5841673fc5c296af0df028e3ba7e8004cb426728
perfectrider <konsgrebenev@gmail.com>
2023-05-14 20:24:04 +0300

    new(app): refact

--- build_app/utils.py
+++ build_app/utils.py
@@ -1,42 +1,34 @@
 import yaml
 
-def get_sorted_tasks(build_name):
-    with open('builds/tasks.yaml', 'r') as f:
-        tasks = yaml.safe_load(f)['tasks']
 
-    with open('builds/builds.yaml', 'r') as f:
+def get_sorted_tasks(build_name):
+    with open('builds/tasks.yaml') as f:
+        tasks = yaml.safe_load(f)['tasks']
+    with open('builds/builds.yaml') as f:
         builds = yaml.safe_load(f)['builds']
 
-    build = None
-    for b in builds:
-        if b['name'] == build_name:
-            build = b
-            break
+    build = next((b for b in builds if b['name'] == build_name), None)
     if not build:
         raise ValueError(f'Build "{build_name}" not found')
 
-    task_dict = {task['name']: task for task in tasks}
-
-    dependency_dict = {}
-    for task in tasks:
-        for dependency in task.get('dependencies', []):
-            if dependency not in dependency_dict:
-                dependency_dict[dependency] = set()
-            dependency_dict[dependency].add(task['name'])
+    dependency_dict = {
+        t['name']: set(d for d in t.get('dependencies', [])) for t in tasks
+    }
 
     sorted_tasks = []
     visited = set()
+
     def visit(task_name):
         if task_name in visited:
             return
         visited.add(task_name)
-        task = task_dict[task_name]
-        for dependency_name in task.get('dependencies', []):
-            visit(dependency_name)
+        for dep_name in dependency_dict.get(task_name, []):
+            visit(dep_name)
         sorted_tasks.append(task_name)
 
     for task_name in build['tasks']:
         visit(task_name)
 
     return sorted_tasks[::-1]
+
 
